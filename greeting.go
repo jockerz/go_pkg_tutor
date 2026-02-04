72b92ff51aeb8e6ce85a8e50284b91ce3aa221e4
user <user@localhost.localdomain>
2026-02-04 13:54:03 +0700

    initial commit

---
+++ greeting.go
@@ -0,0 +1,10 @@
+// package github.com/jockerz/go_pkg_tutor/greeting
+package github.com/jockerz/go_pkg_tutor
+
+
+import "github.com/k0kubun/pp/v3"
+
+
+func Greet(name string) {
+    pp.Fprintf("Greeting %s\n", name)
+}
