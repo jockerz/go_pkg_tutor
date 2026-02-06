c089ccb06dae564ca331ff387e2049acc175a12b
user <user@localhost.localdomain>
2026-02-06 15:58:43 +0700

    pkg documentation added

--- greeting.go
+++ greeting.go
@@ -1,7 +1,13 @@
+// Greeting
+// This is a repo for Golang development demo.
+// This module prints a greet sentence, with color.
+//
+// Greet function usage: `greeting.Greet("Your name")
 package greeting
 
 import "github.com/k0kubun/pp/v3"
 
+// Greet is a function that print greeting message with color
 func Greet(name string) error {
 	pp.Printf("Greeting %s\n", name)
     return nil
